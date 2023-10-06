831f91ecdc41364ff2a94dcef13c3c0012d8f9a9
Ummam Ali (Lappy) <sblue7446@gmail.com>
2023-10-06 18:23:29 +0500

    Handle connections from frontend

--- api.js
+++ api.js
@@ -1,7 +1,10 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
+
 const port = 5500;
 
 const uri =
