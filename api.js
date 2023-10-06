4b05d269eb8219a4a89bf5c3cddc398cd37c4451
Ummam Ali (Lappy) <sblue7446@gmail.com>
2023-10-06 16:54:24 +0500

    First Commit: Only the contributor function is working

---
+++ api.js
@@ -0,0 +1,135 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+
+const app = express();
+const port = 5500;
+
+const uri =
+  "mongodb+srv://prog:prog@cluster0.eeu9yqd.mongodb.net/?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
+app.get("/", async (req, res) => {
+  res.json({
+    msg: "Backend is Running",
+    availableMethods: {
+      "/addContributor?name=####": "Adds contributor to the database",
+      "/allContributors": "Get data about all contributors",
+    },
+  });
+});
+
+// adds a contributor
+app.get("/addContributor", async (req, res) => {
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
+  try {
+    const db = client.db("photons");
+    const contribs = db.collection("contributors");
+    const result = await contribs.insertOne({ name: req.query.name });
+    res.json(result);
+  } catch (e) {
+    res.json({ msg: "error occured", err: e.msg });
+  } finally {
+    await client.close();
+  }
+});
+
+// gets all contributors
+app.get("/allContributors", async (req, res) => {
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
+  try {
+    const db = client.db("photons");
+    const contribs = db.collection("contributors");
+    const ans = await contribs.find().toArray();
+    //const ansNames = ans.map((obj) => obj.name);
+    res.json({ status: 200, ans });
+  } catch (e) {
+    res.json({ msg: "error occured", err: e.msg });
+  } finally {
+    await client.close();
+  }
+});
+
+// adds a funding
+app.get("/addFunding", async (req, res) => {
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
+  try {
+    const db = client.db("photons");
+    const funding = db.collection("fundings");
+    await funding.insertOne({
+      name: req.query.name,
+      amount: req.query.amount,
+      fundId: req.query.fundId,
+    });
+    res.send({ status: 200 });
+  } catch (e) {
+    res.send({ msg: "error occured", err: e.msg });
+  } finally {
+    await client.close();
+  }
+});
+
+// gets all fundings
+app.get("/allFundings", async (req, res) => {
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
+  try {
+    const db = client.db("photons");
+    const fundings = db.collection("fundings");
+    const ans = await fundings.find().toArray();
+    res.send({ status: 200, ans: ans });
+  } catch (e) {
+    res.send({ msg: "error occured", err: e.msg });
+  } finally {
+    await client.close();
+  }
+});
+
+// deletes a funding
+app.get("/deleteFunding", async (req, res) => {
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
+  try {
+    const db = client.db("photons");
+    const fundings = db.collection("fundings");
+    const ans = await fundings.deleteOne({
+      fundId: req.query.fundId,
+    });
+    res.send({ status: 200, deletedCount: ans.deletedCount });
+  } catch (e) {
+    res.send({ msg: "error occured", err: e.msg });
+  } finally {
+    await client.close();
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Backend is listening on port ${port}`);
+});
