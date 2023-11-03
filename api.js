0c524bca2967c1d4e4b71e333e33ffc135c887b4
Ummamali <61016734+Ummamali@users.noreply.github.com>
2023-11-03 23:12:40 +0500

    Update api.js

--- api.js
+++ api.js
@@ -133,6 +133,6 @@ app.get("/deleteFunding", async (req, res) => {
   }
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Backend is listening on port ${port}`);
 });
